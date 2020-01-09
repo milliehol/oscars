b65fbb0082b02beeaf37f2f41f5c228ec0f5c98a
Carla <carla.holcomb25@gmail.com>
2020-01-08 20:18:56 -0500

    Create scraper.rb

---
+++ lib/oscars/scraper.rb
@@ -0,0 +1,20 @@
+
+class Test::Scraper
+
+  def get_page
+    Nokogiri::HTML(open("https://www.filmsite.org/bestactor2.html"))
+  end
+
+  def self.create_actor
+    
+     new_page = Nokogiri::HTML(open("https://www.filmsite.org/bestactor2.html"))
+    
+     new_page.css("div#mainBodyWrapper table tr").each do |actor|
+       year = actor.css("td:nth-child(1)").text.strip
+       name = actor.css("td:nth-child(2)").text.strip
+       role = actor.css("td:nth-child(3)").text.strip
+       Test::Actor.new(year, name, role)
+       end
+  end
+
+end
