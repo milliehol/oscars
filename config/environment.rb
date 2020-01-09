b5405beba9f5091930c12f59bdc6fd83d435e425
Carla <carla.holcomb25@gmail.com>
2020-01-08 20:50:23 -0500

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,6 @@ require 'nokogiri'
 require 'open-uri'
 
 require_relative '../lib/oscars/scraper'
-require_relative '../lib/oscars/actor'
+require_relative '../lib/oscars/oscars'
 require_relative '../lib/oscars/cli'
 require_relative '../lib/oscars/version'
