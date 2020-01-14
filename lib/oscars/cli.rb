d21272ff6f82c27bb21efbcc188b7dac4ddd74e9
Carla <carla.holcomb25@gmail.com>
2020-01-13 20:41:49 -0500

    Update cli.rb

--- lib/oscars/cli.rb
+++ lib/oscars/cli.rb
@@ -15,7 +15,7 @@ class Oscars::CLI
     puts "Please select a year in order to view the actor who won that year.  Please enter the number before the year."
     input = gets.strip.to_i
     
-    if input < 93 && = Integer
+    if input < 93 && == Integer
          actor = Oscars::Actor.find(input)
          display_actor_info(actor)
     else
